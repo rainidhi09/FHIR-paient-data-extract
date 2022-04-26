fd7933ee0dc4fe8b0382092c83f14e5ef151bcdc
Nidhi rai <rainidhi09@gmail.com>
2022-04-26 14:37:04 +0100

    updated doc

--- readme.md
+++ readme.md
@@ -3,10 +3,13 @@
 
 # This repo is to read patient resource FHIR data in json format, process via pyspark(batch process) and store into mysql database
 Two table created in mysql: one for patient personal data other is for patient address data
+Similar to two table we can create more tables as per resource type data being received and populate values with pipeline.
+Driver Class: 
+#execute_paient_data_pipeline.py
 
 ![Paient Table data](https://github.com/rainidhi09/FHIR-paient-data-extract/blob/master/images/Screenshot%202022-04-25%20at%2023.57.07.png "Optional title")
 
 ![Paient Address Table data](https://github.com/rainidhi09/FHIR-paient-data-extract/blob/master/images/Screenshot%202022-04-25%20at%2023.57.49.png "Optional title")
 
-
+#Flow chart
 ![Possible data ingestion pattern](https://github.com/rainidhi09/FHIR-paient-data-extract/blob/master/images/Screenshot%202022-04-26%20at%2000.17.43.png "Possible data ingestion pattern")
--- src/paient_data_process.py
+++ src/paient_data_process.py
@@ -23,6 +23,7 @@ def read_data(spark, path):
 
 
 def transform_name(df):
+    """getting all different types of  names and other information from the row data.  """
     schema = StructType([
         StructField('family', StringType(), True),
         StructField('given', ArrayType(StringType(), True)),
@@ -32,13 +33,14 @@ def transform_name(df):
 
     convertUDF = udf(lambda z: convert_name(z), MapType(StringType(), schema))
 
-    return df.select(col('id'),col('birthdate'), col('gender'), convertUDF(col("name")).alias("newName"))\
+    return df.select(col('id'), col('birthdate'), col('gender'), convertUDF(col("name")).alias("newName"))\
         .withColumn('maiden_name', to_json(col('newName').getItem('maiden_name'))) \
         .withColumn('usual_name', to_json(col('newName').getItem('usual_name'))) \
         .withColumn('official_name', to_json(col('newName').getItem('official_name'))).drop("newName")
 
 
 def transform_address(df):
+    """getting multiple address details from nested value(row data) and putting them into different rows. """
     return df.select(col('id'), explode('address').alias('new_address')) \
         .withColumn('use_add', col('new_address').use) \
         .withColumn('type_add', col('new_address').type) \
@@ -57,6 +59,7 @@ def execute_source_operation(spark, path):
 
 
 def execute_sink_operation(name_df, address_df):
+    """sink data in multiple tables of mysql."""
     name_df.write.format('jdbc').options(
         url='jdbc:mysql://localhost/sql_store',
         driver='com.mysql.jdbc.Driver',
